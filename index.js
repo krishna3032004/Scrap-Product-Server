90be6cae178a798e4f9e2c3d1a7d8a979493fd88
krishna3032004 <krishnagupta30mar@gmail.com>
2025-08-11 13:07:31 +0530

    new project

--- index.js
+++ index.js
@@ -172,7 +172,7 @@ async function scrapeAmazon(url) {
     // await page.goto(url, { waitUntil: 'networkidle2' });
 
     try {
-      await page.waitForSelector('#productTitle', { timeout: 12000 });
+      await page.waitForSelector('#productTitle', { timeout: 20000 });
     } catch {
       console.log("Title not found in time, trying alternative selector...");
     }
