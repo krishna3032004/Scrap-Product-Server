3500cd47b790a46c6af032c578f6ded4328b6c3a
krishna3032004 <krishnagupta30mar@gmail.com>
2025-08-16 13:47:25 +0530

    add readme.md

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ async function safeGotoforamazon(page, url, retries = 3) {
 async function blockExtraResourcesflipkart(page) {
   await page.setRequestInterception(true);
   page.on('request', (req) => {
-    const blocked = ['stylesheet', 'font', 'media','image'];
+    const blocked = ['stylesheet', 'font', 'media'];
     if (blocked.includes(req.resourceType())) {
       req.abort();
     } else { req.continue(); }
@@ -487,8 +487,9 @@ async function scrapeFlipkart(url) {
     console.log("safegoto pai ja rha")
     await safeGoto(page, url);
     console.log("safegoto ho gya")
-    await page.screenshot({ path: "debug.png"});
+    const im =await page.screenshot({ path: "debug.png"});
     console.log("Screenshot captured: debug.png");
+    console.log(im)
 
     // await new Promise(r => setTimeout(r, 5000));
     // // Optional: scroll a bit to avoid lazy-loading
