b8caf72847d3bb4791566afcfe85e051019c3420
krishna3032004 <krishnagupta30mar@gmail.com>
2025-08-16 15:12:13 +0530

    add readme.md

--- index.js
+++ index.js
@@ -439,10 +439,10 @@ app.post('/api/scrape-prices', async (req, res) => {
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122 Safari/537.36"
     );
     console.log("user agent ho gya")
-    await page.setExtraHTTPHeaders({
-      'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8'
-    });
-    console.log("extra http header lg gye")
+    // await page.setExtraHTTPHeaders({
+    //   'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8'
+    // });
+    // console.log("extra http header lg gye")
     // await page.setViewport({ width: 1366, height: 768 });
 
 
